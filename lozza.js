3eae9d6810a7839937d6602d7676921274a5cc3b
Colin Jenkins <op12no2@gmail.com>
2025-05-19 11:43:36 +0100

    Use Leela data and HL size of 384
    [0,5]
    Score of lozza.js vs lozzacand.js: 410 - 276 - 398  [0.562] 1084
    ...      lozza.js playing White: 211 - 136 - 196  [0.569] 543
    ...      lozza.js playing Black: 199 - 140 - 202  [0.555] 541
    ...      White vs Black: 351 - 335 - 398  [0.507] 1084
    Elo difference: 43.2 +/- 16.5, LOS: 100.0 %, DrawRatio: 36.7 %
    SPRT: llr 2.9 (100.4%), lbound -2.25, ubound 2.89 - H1 was accepted

--- lozza.js
+++ lozza.js
@@ -9,6 +9,7 @@ const BUILD = "6";
 
 /*
 
+- Use Leela data and increase hidden layer to 384.
 - Increase default bench depth to 10.
 - Fix formatFen().
 - Fix netPromote() when no captured piece.
@@ -52,7 +53,7 @@ const BUILD = "6";
 //  See https://github.com/op12no2/lozza/wiki/Making-a-release.
 //
 
-const NET_WEIGHTS_FILE = '/home/xyzzy/lozza/nets/teddy168/lozza-934/quantised.bin';
+const NET_WEIGHTS_FILE = '/home/xyzzy/lozza/nets/leela2/lozza-500/quantised.bin';
 const TTSIZE           = 1 << 23;
 const BENCH_DEPTH      = 10;
 
@@ -69,7 +70,7 @@ const NET_QB      = 64;
 const NET_QAB     = NET_QA * NET_QB;
 const NET_SCALE   = 400;
 const NET_I_SIZE  = 768;
-const NET_H1_SIZE = 168;
+const NET_H1_SIZE = 384;
 
 const IMAP = new Uint32Array(15 * 256);
 
