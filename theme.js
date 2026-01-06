88a9aeb5053a44eeab7fca3609c4915f170a5922
op12no2 <op12no2@gmail.com>
2026-01-06 20:53:55 +0000

    Delete theme.js

--- theme.js
+++
@@ -1,6 +0,0 @@
-let themeLeadin       = '#555555';
-let themeNode         = '#3867d6';
-let themeNodeKnob     = '#ffdd00';
-let themeSettingsKnob = '#ff2d55';
-let themeFileKnob     = '#ff2d55';
-let themeButton       = '#ff2d55';
