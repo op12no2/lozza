bf36a2075e1104f3c561f7e3a438a8b9a7db248a
op12no2 <op12no2@gmail.com>
2026-01-22 20:41:30 +0000

    add qs but unused

---
+++ src/qsearch.c
@@ -0,0 +1,86 @@
+#include "types.h"
+#include "builtins.h"
+#include "nodes.h"
+#include "position.h"
+#include "evaluate.h"
+#include "qsearch.h"
+#include "makemove.h"
+#include "timecontrol.h"
+#include "iterate.h"
+#include "move.h"
+
+int qsearch(const int ply, int alpha, const int beta) {
+
+  Node *node = &nodes[ply];
+  const Position *pos = &node->pos;
+
+  if (ply >= MAX_PLY-1) {
+    return evaluate_stm(pos);
+  }
+
+  const int stm = pos->stm;
+  const int opp = stm ^ 1;
+  const int stm_king_idx = piece_index(KING, stm);
+  const int in_check = is_attacked(pos, bsf(pos->all[stm_king_idx]), opp);
+
+  int stand_pat = evaluate_stm(pos);
+
+  if (in_check == 0) {
+    if (stand_pat >= beta) {
+      return beta;
+    }
+    if (stand_pat > alpha) {
+      alpha = stand_pat;
+    }
+  }
+
+  TimeControl *tc = &time_control;
+  tc->nodes++;
+  if ((tc->nodes & 1023) == 0) {
+    check_tc();
+    if (tc->finished)
+      return 0;
+  }
+
+  Node *next_node = &nodes[ply + 1];
+  Position *next_pos = &next_node->pos;
+  move_t move;
+  int score;
+  int best_score = in_check ? -INF : stand_pat;
+  int num_legal_moves = 0;
+  const uint64_t *next_stm_king_ptr = &next_pos->all[stm_king_idx];
+
+  init_next_qsearch_move(node, in_check, 0);
+
+  while ((move = get_next_qsearch_move(node))) {
+
+    pos_copy(pos, next_pos);
+    make_move(next_pos, move);
+    if (is_attacked(next_pos, bsf(*next_stm_king_ptr), opp))
+      continue;
+
+    num_legal_moves++;
+
+    score = -qsearch(ply+1, -beta, -alpha);
+
+    if (tc->finished)
+      return 0;
+
+    if (score > best_score) {
+      best_score = score;
+      if (score > alpha) {
+        alpha = score;
+        if (score >= beta) {
+          return score;
+        }
+      }
+    }
+  }
+
+  if (in_check && num_legal_moves == 0) {
+    return -MATE + ply;
+  }
+
+  return best_score;
+
+}
---
+++ src/qsearch.h
@@ -0,0 +1,6 @@
+#ifndef QSEARCH_H
+#define QSEARCH_H
+
+int qsearch(const int ply, int alpha, const int beta);
+
+#endif
